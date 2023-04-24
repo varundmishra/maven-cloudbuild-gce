12d043da71eedfeae170d0f12256ed96529c1e92
varundmishra <varundmishra@google.com>
2023-04-24 08:26:00 +0000

    updated to v8 with pom to 1.0.2

--- src/main/java/com/app/helloworld/App.java
+++ src/main/java/com/app/helloworld/App.java
@@ -20,6 +20,6 @@ public class App extends SpringBootServletInitializer {
 
    @RequestMapping(value = "/")
    public String hello() {
-      return "<center>Hello World v7</center>";
+      return "<center>Hello World v8</center>";
    }
 }
