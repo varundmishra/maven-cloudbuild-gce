002a35fb1316fabf2cc9ef194c2620de86912ef7
varundmishra <varundmishra@google.com>
2023-04-24 07:27:28 +0000

    updated to v5

--- src/main/java/com/app/helloworld/App.java
+++ src/main/java/com/app/helloworld/App.java
@@ -20,6 +20,6 @@ public class App extends SpringBootServletInitializer {
 
    @RequestMapping(value = "/")
    public String hello() {
-      return "<center>Hello World v4</center>";
+      return "<center>Hello World v5</center>";
    }
 }
