98f01329f5232fa6fc2e4d52fe43f0c737409900
varundmishra <varundmishra@google.com>
2023-04-24 07:09:32 +0000

    updated to v3, added mvn deploy

--- src/main/java/com/app/helloworld/App.java
+++ src/main/java/com/app/helloworld/App.java
@@ -20,6 +20,6 @@ public class App extends SpringBootServletInitializer {
 
    @RequestMapping(value = "/")
    public String hello() {
-      return "<center>Hello World v2</center>";
+      return "<center>Hello World v3</center>";
    }
 }
